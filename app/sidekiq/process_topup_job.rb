33e0cbf4098132ad2ea2979d0a90a6bcd2ba3cfe
Marcos Teixeira <m.viniteixeira@gmail.com>
2022-07-04 14:23:07 +0100

    Add wallet column

--- app/sidekiq/process_topup_job.rb
+++ app/sidekiq/process_topup_job.rb
@@ -6,11 +6,22 @@ class ProcessTopupJob
 
     network, wallet = destination.split(':') # "matic:0x7569c080aFBeE7A2f0017865a214E2f7A416F719"
     
-    referral = Referral.where(wallet: wallet)
+    referral = Referral.includes(:referral_code)
+                       .where(wallet: wallet)
                        .where('created_at < (?)', Time.at(timestamp))
                        .first
     return unless referral
 
-    Reward.create(uid: topup_id, referral_id: referral.id, source: source, claimed: false)
+    return if Reward.joins(referral: :referral_code)
+                    .where(wallet: wallet)
+                    .where.not(referral_code: { wallet: wallet })
+                    .any?
+
+    Reward.transaction do
+      Reward.create(uid: topup_id, referral_id: referral.id, source: source,
+                    claimed: false, wallet: wallet)
+      Reward.create(uid: topup_id, referral_id: referral.id, source: source,
+                    claimed: false, wallet: referral.referral_code.wallet)
+    end
   end
 end
--- spec/sidekiq/process_topup_job_spec.rb
+++ spec/sidekiq/process_topup_job_spec.rb
@@ -16,10 +16,15 @@ describe ProcessTopupJob, type: :job do
       context 'when the referral exists' do
         let(:referral) { create(:referral, created_at: 1.week.ago) }
 
-        it 'creates a reward' do
+        it 'creates two rewards' do
           expect do
             subject.perform(1, "matic:#{referral.wallet}", Time.now.to_i)
-          end.to change { Reward.count }.by(1)
+          end.to change { Reward.count }.by(2)
+        end
+
+        it 'creates the correct rewards' do
+          subject.perform(1, "matic:#{referral.wallet}", Time.now.to_i)
+          expect(Reward.where(wallet: [referral.wallet, referral.referral_code.wallet]).count).to eq(2)
         end
 
         context 'with a referral after the topup' do
@@ -37,6 +42,35 @@ describe ProcessTopupJob, type: :job do
             subject.perform(1, "matic:0xabcd", Time.now.to_i)
           end.to_not change { Reward.count }
         end
+      end
+    end
+
+    context 'when the referred already has a reward' do
+      let(:referral) { create(:referral, created_at: 1.week.ago) }
+
+      it 'does not give another reward' do
+        expect do
+          subject.perform(1, "matic:#{referral.wallet}", Time.now.to_i)
+        end.to change { Reward.count }.by(2)
+
+        expect do
+          subject.perform(2, "matic:#{referral.wallet}", Time.now.to_i)
+        end.to_not change { Reward.count }
+      end
+    end
+
+    context 'when the referral already has a reward' do
+      let(:referral) { create(:referral, created_at: 1.week.ago) }
+      let(:another) { create(:referral, referral_code: referral.referral_code, created_at: 1.week.ago) }
+
+      it 'gives another reward' do
+        expect do
+          subject.perform(1, "matic:#{referral.wallet}", Time.now.to_i)
+        end.to change { Reward.count }.by(2)
+
+        expect do
+          subject.perform(2, "matic:#{another.wallet}", Time.now.to_i)
+        end.to change { Reward.count }.by(2)
       end
     end
   end
